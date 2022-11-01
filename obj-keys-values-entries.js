0ca3e802509236ffb31f8191d0241d84dc3975b4
hyeongwookimdev <hyeongwookim.dev@gmail.com>
2022-11-01 12:53:12 +0900

    add: example obj-keys-values-entries.js

---
+++ obj-keys-values-entries.js
@@ -0,0 +1,9 @@
+const user = {
+  username: "Daegil",
+  id: 1,
+  level: 11,
+};
+
+console.log(Object.keys(user)); // [ 'username', 'id', 'level' ]
+console.log(Object.values(user)); // [ 'Daegil', 1, 11 ]
+console.log(Object.entries(user)); // [ [ 'username', 'Daegil' ], [ 'id', 1 ], [ 'level', 11 ] ]
